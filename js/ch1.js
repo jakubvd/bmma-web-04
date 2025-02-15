5c6aa0e8c72385fecf76ef7769b932f891fea37f
jakubvd <jakub.michalak@visiondevs.pl>
2025-02-15 23:31:21 +0100

    ad

--- js/ch1.js
+++ js/ch1.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     // Check if the screen width is above a certain threshold (e.g., 992p or above)
     if (window.innerWidth >= 992) {
-        $(".layout423_card").hover(function() {
+        $(".hover_card").hover(function() {
             // When a card is hovered over
-            $(".layout423_card-content").not($(this).find(".layout423_card-content")).addClass("inactive");
+            $(".hover_card_card-content").not($(this).find(".hover_card_card-content")).addClass("inactive");
         }, function() {
             // When the mouse leaves a card
-            $(".layout423_card-content").removeClass("inactive");
+            $(".hover_card_card-content").removeClass("inactive");
         });
     }
 });
