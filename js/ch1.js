0074764ce2fcae3d3b7b55e1127ae0c74092420d
jakubvd <jakub.michalak@visiondevs.pl>
2025-02-15 17:33:06 +0100

    Create ch1.js

---
+++ js/ch1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Check if the screen width is above a certain threshold (e.g., 768px)
+    if (window.innerWidth >= 992) {
+        $(".layout423_card").hover(function() {
+            // When a card is hovered over
+            $(".layout423_card-content").not($(this).find(".layout423_card-content")).addClass("inactive");
+        }, function() {
+            // When the mouse leaves a card
+            $(".layout423_card-content").removeClass("inactive");
+        });
+    }
+});
